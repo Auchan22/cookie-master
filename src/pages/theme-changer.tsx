777b11ae6a677fe388e8e89bc325bf17d60a90a7
Agustin <agustinsurila@gmail.com>
2023-04-12 17:55:35 -0300

    Handling Change Theme

--- src/pages/theme-changer.tsx
+++ src/pages/theme-changer.tsx
@@ -1,8 +1,44 @@
+import {
+  Card,
+  CardContent,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+} from '@mui/material';
 import { NextPage } from 'next';
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 
 const ThemeChangerPage: NextPage = () => {
-  return <div>ThemeChangerPage</div>;
+  const [currentTheme, setCurrentTheme] = useState<string>('light');
+
+  const onThemeChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setCurrentTheme(e.target.value);
+  };
+
+  return (
+    <Card>
+      <CardContent>
+        <FormControl>
+          <FormLabel>Tema</FormLabel>
+          <RadioGroup
+            value={currentTheme}
+            defaultValue='light'
+            onChange={onThemeChange}
+          >
+            <FormControlLabel value='light' control={<Radio />} label='Light' />
+            <FormControlLabel value='dark' control={<Radio />} label='Dark' />
+            <FormControlLabel
+              value='custom'
+              control={<Radio />}
+              label='Custom'
+            />
+          </RadioGroup>
+        </FormControl>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default ThemeChangerPage;
