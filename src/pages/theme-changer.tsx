1aab6686fc81c03af43989d016f306b50db966cf
Agustin <agustinsurila@gmail.com>
2023-04-12 18:06:02 -0300

    Empezando con las cookies 🍪

--- src/pages/theme-changer.tsx
+++ src/pages/theme-changer.tsx
@@ -8,25 +8,31 @@ import {
   RadioGroup,
 } from '@mui/material';
 import { NextPage } from 'next';
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
+
+import Cookies from 'js-cookie';
 
 const ThemeChangerPage: NextPage = () => {
   const [currentTheme, setCurrentTheme] = useState<string>('light');
 
   const onThemeChange = (e: ChangeEvent<HTMLInputElement>) => {
     setCurrentTheme(e.target.value);
+    localStorage.setItem('theme', e.target.value);
+    Cookies.set('theme', e.target.value);
   };
+
+  //Las cookies tienen solo 4k de almacenamiento, pero permiten enviarlas al back en requestTime
+
+  useEffect(() => {
+    console.log(localStorage.getItem('theme'));
+  }, []);
 
   return (
     <Card>
       <CardContent>
         <FormControl>
           <FormLabel>Tema</FormLabel>
-          <RadioGroup
-            value={currentTheme}
-            defaultValue='light'
-            onChange={onThemeChange}
-          >
+          <RadioGroup value={currentTheme} onChange={onThemeChange}>
             <FormControlLabel value='light' control={<Radio />} label='Light' />
             <FormControlLabel value='dark' control={<Radio />} label='Dark' />
             <FormControlLabel
